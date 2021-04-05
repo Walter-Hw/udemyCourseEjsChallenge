0e16997a7c6d08ba794ae13736ffafada8334e42
Walter Huang <autumnhao@gmail.com>
2021-04-05 16:02:55 -0700

    User now can type personal messages in compose page and render it to home page.

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-let posts = [];
 
 const homeStartingContent = `Cupidatat dolor ex proident aliqua ex ad nulla velit quis labore laboris tempor. Excepteur sunt amet anim non sunt labore nisi adipisicing id tempor elit commodo. Ea id dolore voluptate reprehenderit irure fugiat elit adipisicing sit. Consequat commodo eiusmod aliqua est minim occaecat quis elit excepteur eiusmod et id.`;
 
@@ -8,12 +7,15 @@ const aboutContent = `Amet eu laboris ipsum elit id mollit exercitation incididu
 
 const contactContent = `Est amet labore culpa dolor irure in sunt ut do aliqua id pariatur cillum incididunt. Qui sit fugiat fugiat occaecat qui cillum et magna est ipsum. Laborum exercitation incididunt in sit laboris ea quis quis deserunt magna occaecat.`;
 
+let posts = [];
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
+
 
   res.render('home', {
     homeText: homeStartingContent, 
